1bdb63cbd9672b5fe8d764c9b88261d2648ebd19
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2025-04-10 09:10:21 +0530

    Create Officer.java

---
+++ com.hexawareTraining.crimeAnalysisAndReportingSystem/entity/Officer.java
@@ -0,0 +1,67 @@
+package com.hexawareTraining.crimeAnalysisAndReportingSystem.entity;
+
+public class Officer {
+    private int officerId;
+    private String firstName;
+    private String lastName;
+    private String badgeNumber;
+    private int officerRank; 
+    private String contactInformation;
+    private int agencyId;
+	public Officer(int officerId, String firstName, String lastName, String badgeNumber, int officerRank,
+			String contactInformation, int agencyId) {
+		super();
+		this.officerId = officerId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.badgeNumber = badgeNumber;
+		this.officerRank = officerRank;
+		this.contactInformation = contactInformation;
+		this.agencyId = agencyId;
+	}
+	public int getOfficerId() {
+		return officerId;
+	}
+	public void setOfficerId(int officerId) {
+		this.officerId = officerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getBadgeNumber() {
+		return badgeNumber;
+	}
+	public void setBadgeNumber(String badgeNumber) {
+		this.badgeNumber = badgeNumber;
+	}
+	public int getOfficerRank() {
+		return officerRank;
+	}
+	public void setOfficerRank(int officerRank) {
+		this.officerRank = officerRank;
+	}
+	public String getContactInformation() {
+		return contactInformation;
+	}
+	public void setContactInformation(String contactInformation) {
+		this.contactInformation = contactInformation;
+	}
+	public int getAgencyId() {
+		return agencyId;
+	}
+	public void setAgencyId(int agencyId) {
+		this.agencyId = agencyId;
+	}
+    
+    
+	
+}
