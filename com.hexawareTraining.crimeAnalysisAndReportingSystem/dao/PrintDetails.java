37ff3ae9f10373abdbb19e34ca3e092d020f756f
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2025-04-10 09:14:53 +0530

    Create PrintDetails.java

---
+++ com.hexawareTraining.crimeAnalysisAndReportingSystem/dao/PrintDetails.java
@@ -0,0 +1,11 @@
+package com.hexawareTraining.crimeAnalysisAndReportingSystem.dao;
+
+import java.util.List;
+
+import com.hexawareTraining.crimeAnalysisAndReportingSystem.entity.Incident;
+import com.hexawareTraining.crimeAnalysisAndReportingSystem.entity.Report;
+
+public interface PrintDetails {
+	void printIncidentReport(Report report);
+	void printIncdients(List<Incident> incidents);
+}
