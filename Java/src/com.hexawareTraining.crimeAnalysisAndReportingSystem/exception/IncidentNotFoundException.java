6e3368db92b512b98e9541c61500898f99bb9f69
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2025-04-13 21:56:30 +0530

    Create IncidentNotFoundException.java

---
+++ Java/src/com.hexawareTraining.crimeAnalysisAndReportingSystem/exception/IncidentNotFoundException.java
@@ -0,0 +1,9 @@
+package com.hexawareTraining.crimeAnalysisAndReportingSystem.exception;
+
+public class IncidentNotFoundException extends RuntimeException{
+	public IncidentNotFoundException(String messaage){
+		super(messaage);
+		
+	}
+	
+}
