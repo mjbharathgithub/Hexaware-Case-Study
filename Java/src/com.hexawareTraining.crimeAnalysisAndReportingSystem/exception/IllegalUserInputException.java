5dd4e403336f0af9242b78affea5106dac839261
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2025-04-13 21:56:09 +0530

    Create IllegalUserInputException.java

---
+++ Java/src/com.hexawareTraining.crimeAnalysisAndReportingSystem/exception/IllegalUserInputException.java
@@ -0,0 +1,11 @@
+package com.hexawareTraining.crimeAnalysisAndReportingSystem.exception;
+
+public class IllegalUserInputException extends RuntimeException {
+	
+	public IllegalUserInputException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+		
+	}
+
+}
