5270aad60c3c13992c72c7769eca787bc4a27fcc
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2025-04-14 21:47:46 +0530

    Update GetInputImpl.java

--- Java/src/com.hexawareTraining.crimeAnalysisAndReportingSystem/dao/GetInputImpl.java
+++ Java/src/com.hexawareTraining.crimeAnalysisAndReportingSystem/dao/GetInputImpl.java
@@ -47,19 +47,16 @@ public class GetInputImpl implements GetInput {
         System.out.print("Description: ");
         String description = scanner.nextLine();
 
-        System.out.print("Status (Open,closed or Under Investigation) : ");
-        String status = scanner.nextLine();
-        if(!checkStatus(status)) {
-        	throw new IllegalUserInputException(status+" is not accepted as Status for Incidents");
-        }
+//        System.out.print("Status (Open,closed or Under Investigation) : ");
+        String status = getStatus();
 
         System.out.print("Victim ID: ");
-        int victimId = scanner.nextInt();
-        scanner.nextLine(); 
+        int victimId = Integer.parseInt(scanner.nextLine());
+//        scanner.nextLine(); 
 
         System.out.print("Suspect ID: ");
-        int suspectId = scanner.nextInt();
-        scanner.nextLine(); 
+        int suspectId = Integer.parseInt(scanner.nextLine());
+//        scanner.nextLine(); 
         
 //        if(!pattern.matcher(dateString).matches() || ) {
 //        	return null;
@@ -80,31 +77,36 @@ public class GetInputImpl implements GetInput {
 		}
 		catch (Exception e) {
 			// TODO: handle exception
-			System.err.println("Improper Input Try Again");
+			System.err.println("Improper Input Try Again tihs");
 		}
 		return null;
                   
 	}
 	
-     private boolean checkStatus(String status) {
-    	 if(!(status.equalsIgnoreCase("Open")||status.equalsIgnoreCase("Closed")||status.equalsIgnoreCase("Under Investigation"))) {
-         	return false;
+     private String getStatus() throws IllegalUserInputException {
+    	System.out.println("Enter Status (Open, Closed or Under Investigation) : ");
+ 		String status=scanner.nextLine();
+ 		if((status.equalsIgnoreCase("Open")||status.equalsIgnoreCase("Closed")||status.equalsIgnoreCase("Under Investigation"))) {
+         	return status;
          }
-    	 return false;
+ 		else {throw new IllegalUserInputException(status+" is not accepted as Status for Incidents");
+ 		}
+ 		
+//    	 return "";
 		
 	}
 	
 	public String[] updateIncidentStatusInput() {
 		try {
 		System.out.println("Incident Status Update");
-		System.out.println("Enter Status : ");
-		String status=scanner.nextLine();
-		if(!checkStatus(status)) {
-        	throw new IllegalUserInputException(status+" is not accepted as Status for Incidents");
-        }
+//		System.out.println("Enter Status (Open, Closed or Under Investigation : ");
+		
+		
 		System.out.println("Enter Incient ID : ");
-		int incidentId=scanner.nextInt();
-		scanner.nextLine();
+		int incidentId=Integer.parseInt(scanner.nextLine());
+		String status=getStatus();
+		
+//		scanner.nextLine();
 		
 		
 //		System.out.println("INput read successfully");
@@ -160,8 +162,8 @@ public class GetInputImpl implements GetInput {
 		System.out.println("Searching Options \n1. Incident ID \n2. Incident Type \n3. Location \n4. Status\n5. exit\n>>");
 		String criteria;
 		
-		int option=scanner.nextInt();
-		scanner.nextLine();
+		int option=Integer.parseInt(scanner.nextLine());
+//		scanner.nextLine();
 		switch (option) {
 		case 1:
 			System.out.println("Enter Incident ID : ");
@@ -173,9 +175,11 @@ public class GetInputImpl implements GetInput {
 			System.out.println("Enter Incident Location : ");
 			break;
 		case 4:
-			System.out.println("Enter Incident Status : ");
-			criteria=scanner.nextLine();
-			if(checkStatus(criteria)) return criteria;
+//			System.out.println("Enter Incident Status : ");
+			criteria=getStatus();
+			if(!criteria.equals(""))
+				
+				return criteria;
 			
 			throw new IllegalUserInputException(criteria+" is not accepted as Status for Incidents");
 			
@@ -210,7 +214,7 @@ public class GetInputImpl implements GetInput {
 //		System.out.println("Report Generation");
 		
 		System.out.println("Enter Incident ID : ");
-		int incidentId=scanner.nextInt();
+		int incidentId=Integer.parseInt(scanner.nextLine());
 		
 		System.out.println();
 		Incident incident = new Incident();
@@ -237,35 +241,46 @@ public class GetInputImpl implements GetInput {
 	@Override
 	public int getCaseByIdInput() {
 		System.out.println("Case Detail by ID\nEnter Case ID : ");
-		
-		int searchId= scanner.nextInt();
-		scanner.nextLine();
+		try {
+		int searchId= Integer.parseInt(scanner.nextLine());
+//		scanner.nextLine();
 		return searchId;
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			System.err.println("Improper Input\nTry Again");
+		}
+		
+		return 0;
 		
 	}
 	
 	@Override
-	public void updateCaseInput(Case case1) {
+	public boolean updateCaseInput(Case case1) {
 		
 //		int searchId= getCaseByIdInput();
 //		scanner.nextLine();
 //		Case case1= processor.getCaseDetails(searchId);
+		try {
 		if(case1==null)
 			{
 			System.out.println("No Case Found with ID "+case1.getCaseId());
-			return ;
+			return false;
 			}
 //		case_title = ?, case_description = ?, status = ?
-		System.out.println("Enter new Case Title");
+		System.out.println("Enter new Case Title : ");
 		String newCaseTitle=scanner.nextLine();
 		
-		System.out.println("Enter new Case Description");
+		System.out.println("Enter new Case Description : ");
 		String newCaseDescription=scanner.nextLine();
 		
-		System.out.println("Enter new Case Status");
-		String newCaseStatus=scanner.nextLine();
+//		System.out.println("Enter new Case Status : (Open, Closed or Under Investigation) : ");
+		String newCaseStatus=getStatus();
 		
-		case1.setCaseTitle(newCaseTitle);
+		
+		
+		
+		case1.setCaseTitle(newCaseTitle); 
 		case1.setCaseDescription(newCaseDescription);
 		case1.setStatus(newCaseStatus);
 		System.out.println("Do You want to Add Incident to the Case (yes/no) : ");
@@ -273,7 +288,18 @@ public class GetInputImpl implements GetInput {
 		if(addIncidentOption.equalsIgnoreCase("yes")) {
 			processor.addIncident(case1.getIncidents(), this, output);
 		}
-		System.out.println("Case is "+(processor.updateCaseDetails(case1)?"Updated":"Not Updated"));
+		
+		
+		return processor.updateCaseDetails(case1);
+		}catch (IllegalUserInputException e) {
+			// TODO: handle exception
+			System.err.println(e.getLocalizedMessage()+"\nTry Again");
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 }
