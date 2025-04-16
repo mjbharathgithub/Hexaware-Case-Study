79ee345bbedc1f0d131949191f23dc0e8534cac3
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2025-04-16 23:10:47 +0530

    Update CrimeAnalysisService.java

--- Java/src/com.hexawareTraining.crimeAnalysisAndReportingSystem/dao/CrimeAnalysisService.java
+++ Java/src/com.hexawareTraining.crimeAnalysisAndReportingSystem/dao/CrimeAnalysisService.java
@@ -1,14 +1,27 @@
 package com.hexawareTraining.crimeAnalysisAndReportingSystem.dao;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Set;
 
 import com.hexawareTraining.crimeAnalysisAndReportingSystem.entity.Case;
+import com.hexawareTraining.crimeAnalysisAndReportingSystem.entity.Evidence;
 import com.hexawareTraining.crimeAnalysisAndReportingSystem.entity.Incident;
+import com.hexawareTraining.crimeAnalysisAndReportingSystem.entity.LawAgency;
+import com.hexawareTraining.crimeAnalysisAndReportingSystem.entity.Officer;
 import com.hexawareTraining.crimeAnalysisAndReportingSystem.entity.Report;
+import com.hexawareTraining.crimeAnalysisAndReportingSystem.entity.Suspect;
+import com.hexawareTraining.crimeAnalysisAndReportingSystem.entity.Victim;
+import com.hexawareTraining.crimeAnalysisAndReportingSystem.service.GetInputImpl;
+import com.hexawareTraining.crimeAnalysisAndReportingSystem.service.PrintDetailsImpl;
 
 public interface CrimeAnalysisService {
+	
+	// Given Implementations
 	
 	boolean createIncident(Incident incident) ;
 	
@@ -27,12 +40,28 @@ public interface CrimeAnalysisService {
 	boolean updateCaseDetails(Case caseObj);
 
 	List<Case> getAllCases();
-
-//	void addIncident(List<Incident> incidents, GetInput input, CrimeAnalysisServiceImpl processor,
-//			PrintDetails output);
-
-	void addIncident(List<Incident> incidents, GetInput input, PrintDetails output);
 	
+	// Additional Implementations
+
+	int addSuspect(Suspect suspect);
+
+	int addVictim(Victim victim);
+
+	int addEvidence(Evidence evidence);
+
+	int createOfficer(Officer officer);
+
+	int createLawAgency(LawAgency agency);
+
+	Incident extractIncidentFromResultSet(ResultSet rs) throws SQLException;
+
+	String extractReportDetailsFromResultSet(ResultSet rs) throws SQLException;
+
+	void addIncident(LinkedHashSet<Incident> incidents, GetInputImpl input, PrintDetailsImpl output);
+
+	List<Incident> filterIncidents(Set<Integer> filterIDs, List<Incident> incidents);
+
+
 	
 
 }
