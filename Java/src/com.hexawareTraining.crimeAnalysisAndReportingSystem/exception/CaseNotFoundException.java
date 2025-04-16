c1f13591d1244c55500a0394c1b3a4611c2b3234
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2025-04-16 23:12:07 +0530

    Create CaseNotFoundException.java

---
+++ Java/src/com.hexawareTraining.crimeAnalysisAndReportingSystem/exception/CaseNotFoundException.java
@@ -0,0 +1,9 @@
+package com.hexawareTraining.crimeAnalysisAndReportingSystem.exception;
+
+public class CaseNotFoundException extends RuntimeException {
+	public CaseNotFoundException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
