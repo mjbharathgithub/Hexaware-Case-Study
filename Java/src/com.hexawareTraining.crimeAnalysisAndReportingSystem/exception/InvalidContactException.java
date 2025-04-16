0ffd62539432407afd0edda4470a68d546b6d1dd
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2025-04-16 23:12:29 +0530

    Create InvalidContactException.java

---
+++ Java/src/com.hexawareTraining.crimeAnalysisAndReportingSystem/exception/InvalidContactException.java
@@ -0,0 +1,7 @@
+package com.hexawareTraining.crimeAnalysisAndReportingSystem.exception;
+
+public class InvalidContactException extends Exception {
+    public InvalidContactException(String message) {
+        super(message);
+    }
+}
